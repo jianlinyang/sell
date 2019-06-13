8a4e5d8c075e3bee874af77a4fe23c4f93121b2b
yang <364334437@qq.com>
2019-06-13 17:08:12 +0800

    初始化

---
+++ src/main/java/com/shu/sell/SellApplication.java
@@ -0,0 +1,16 @@
+package com.shu.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yang
+ */
+@SpringBootApplication
+public class SellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellApplication.class, args);
+    }
+
+}
