f963275b220a7728eb15f3c263544677c88b8332
yang <364334437@qq.com>
2019-06-27 15:22:04 +0800

    list controller

--- src/main/java/com/shu/sell/controller/BuyerOrderController.java
+++ src/main/java/com/shu/sell/controller/BuyerOrderController.java
@@ -9,11 +9,12 @@ import com.shu.sell.service.OrderService;
 import com.shu.sell.utils.ResultVoUtils;
 import com.shu.sell.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -60,7 +61,26 @@ public class BuyerOrderController {
         return ResultVoUtils.success(map);
     }
 
-    //订单列表
+    /**
+     * 订单列表
+     *
+     * @param openId openid
+     * @param page   page
+     * @param size   size
+     * @return {@link ResultVO}
+     */
+    @GetMapping("/list")
+    public ResultVO list(@RequestParam("openId") String openId,
+                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openId)) {
+            log.error("查询订单列表 openId为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest pageRequest = new PageRequest(page, size);
+        Page<OrderDTO> list = orderService.findList(openId, pageRequest);
+        return ResultVoUtils.success(list.getContent());
+    }
 
     //订单详情
 
